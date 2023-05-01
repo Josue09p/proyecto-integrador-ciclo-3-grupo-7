b567338e80bb8edd91b2e5d74ae51a15fa0ffaae
Josue09p <132235530+Josue09p@users.noreply.github.com>
2023-05-01 01:40:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// Seleccionar el elemento HTML con el id "mi-elemento"
+var miElemento = document.getElementById("mi-elemento");
+
+// Cambiar el contenido del elemento HTML
+miElemento.innerHTML = "¡Hola, Mundo!";
