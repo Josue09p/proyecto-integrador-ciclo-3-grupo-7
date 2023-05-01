f2107a43ea3d32882cfed881bd3aa5b062603ce4
Josue <132235530+Josue09p@users.noreply.github.com>
2023-05-01 17:53:11 -0500

    Actualice todo jiji

--- index.js
+++
@@ -1,5 +0,0 @@
-// Seleccionar el elemento HTML con el id "mi-elemento"
-var miElemento = document.getElementById("mi-elemento");
-
-// Cambiar el contenido del elemento HTML
-miElemento.innerHTML = "¡Hola, Mundo!";
